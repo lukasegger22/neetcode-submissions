4d9bb0fecd1f50b69d0d1fe9f50dd51226008cc1
Lukas Egger <lukasegger14@icloud.com>
2026-07-13 14:31:31 +0200

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        minimum = prices[0]
+        result = 0
+        for price in prices:
+            if price < minimum:
+                minimum = price
+                continue
+            calc = price - minimum
+            if calc  > result:
+                result = calc 
+        return result
+        
