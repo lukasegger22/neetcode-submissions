40209cbbf861aa488f80e66db85824969c32e8b6
Lukas Egger <lukasegger14@icloud.com>
2026-09-12 15:21:18 +0200

    Add: time-based-key-value-store - submission-18

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-18.py
@@ -0,0 +1,26 @@
+class TimeMap:
+
+    def __init__(self):
+        self.timemap = {}
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key not in self.timemap:
+            self.timemap[key] = []
+        self.timemap[key].append([value, timestamp])
+
+    def get(self, key: str, timestamp: int) -> str:
+        if key not in self.timemap:
+            return ""
+        res = ""
+        left = 0
+        right = len(self.timemap[key])-1
+        while left <= right:
+            mid = left + (right - left) //2
+            if self.timemap[key][mid][1] == timestamp:
+                return self.timemap[key][mid][0]
+            elif self.timemap[key][mid][1] < timestamp:
+                res = self.timemap[key][mid][0]
+                left = mid+1
+            else:
+                right = mid-1
+        return res
